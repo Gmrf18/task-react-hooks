4c1f98e56c0c9976629b18b0d8674dfffb7ecf2f
GmrfDesk-U <gmrf18@outlook.com>
2021-01-24 19:04:59 -0600

    base tasks list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.scss";
+import { Tasks } from "./components/Tasks";
+import { TaskDetail } from "./components/TaskDetail";
+import { TaskModal } from "./components/TaskModal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Tasks />
+      <TaskDetail />
+      {false && <TaskModal />}
+    </Fragment>
   );
 }
 
---
+++ src/components/TaskDetail.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const TaskDetail = () => {
+  return (
+    <div className="TaskDetail">
+      <h3>Buy coffe</h3>
+      <select>
+        <option>uno</option>
+      </select>
+      <h5>Description</h5>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quia
+        possimus dicta et nemo asperiores ea quibusdam delectus id eligendi
+        provident, odit nesciunt expedita beatae iure! Debitis mollitia facilis
+        vel.
+      </p>
+      <br />
+      <p>Updated Today , 6:30pm by Gerardo Reyes</p>
+    </div>
+  );
+};
